72918c66c5a0d47fc89e0c78fdfdfcc22fc2a5b2
DariaRusovich <rusovichdasha41@gmail.com>
2021-09-07 00:31:04 +0300

    creating-function-add-todo-and-context

--- src/components/AddTodoForm.jsx
+++ src/components/AddTodoForm.jsx
@@ -1,33 +1,34 @@
+
+
+
+//ОНА ДОЛЖНА ОТКРЫВАТЬСЯ??
 import { useState } from "react";
 import { useTodos } from "../hooks/useTodos";
 
-
-
-
 export default function AddTodoForm() {
-
-  const [todos, dispatchTodos] = useTodos()
-  const [body, setBody] = useState()
- 
+  const [todos, dispatchTodos] = useTodos();
+  const [body, setBody] = useState(""); //строка будет изменяться?
 
   function addNewTodo(event) {
-    event.preventDefault()
+    event.preventDefault();
+    console.log(body);
+    setBody("") //очищаем форму
+    dispatchTodos({type: 'ADD', payload: {body}})
   }
-
-
 
   return (
     <form onSubmit={addNewTodo} className="todo-form">
-          <input
-          className="checkbox-field"
-          type="checkbox"
-          name="flag"
-          required
-        />
-      <input name="body" type="text" className="field" placeholder="Some task"/>
-   
-        
-      
+      <input className="checkbox-field" type="checkbox" name="flag" required />
+      <input
+        name="body"
+        type="text"
+        className="field"
+        placeholder="Add some task"
+        autoComplete="off"
+        required
+        value={body}
+        onChange={(e) => setBody(e.target.value)}
+      />
     </form>
   );
 }
--- src/hooks/useTodos.js
+++ src/hooks/useTodos.js
@@ -13,13 +13,25 @@ export function useTodos() {
 //context, in order to not to do it every time, we need create custom hook
 export function TodosProvider({children}) {
     const [state, dispatch] = useReducer(reducer, inisialState);
+    useEffect(() => {
+        console.log('todos context is', state);
+    }, [state])
     return <TodosContext.Provider value={[state, dispatch]}>{children}</TodosContext.Provider>
 }
 //The TodosProvider() function is a wrapper function which gets all child elements, components
 //also created useState to get state Todos so that it can change (not simple Todos)
 function reducer(state, action) {
     switch (action.type) {
-   
+        case 'ADD': {
+            const newTodo = {
+                id: Date.now(),
+                body: action.payload.body,
+                createdAt: Date.now(),
+                updatedAt: null,
+                status: 1
+            }
+            return [...state, newTodo]
+        }
         default:
         throw new Error(`Wrong actiot type! (${action.type})`);
     }
