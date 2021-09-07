5d8c6d5bc257cc5ffc7c37b69ea922dacda5b38a
DariaRusovich <rusovichdasha41@gmail.com>
2021-09-07 15:02:22 +0300

    add-todo-statuses

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,8 +2,13 @@
 
 
 export default function Todo({todo}){
+    const todoStatuses = {
+        1: "new",
+        2: "process",
+        3: "done"
+    }
     return(
-        <div className="todo todo-field">
+        <div className={`todo todo-field ${todoStatuses[todo.status]}`} >
             <input type="checkbox" />
              <div className="todo-item-wraper">
              <label htmlFor="checkbox" className="todo-body">{todo.body}</label>
